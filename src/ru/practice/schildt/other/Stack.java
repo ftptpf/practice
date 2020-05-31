312814b014981b2c1611a0a3522cefaa18b44ab1
ftptpf <Nb49Rtdd>
2020-05-31 21:31:56 +0300

    Stack пример

---
+++ src/ru/practice/schildt/other/Stack.java
@@ -0,0 +1,24 @@
+package ru.practice.schildt.other;
+
+class Stack {
+    int[] stck = new int[10];
+    int tos;
+    Stack() {
+        tos = -1;
+    }
+    void push(int item) {
+        if (tos == 9) {
+            System.out.println("Стек заполнен.");
+        } else {
+            stck[++tos] = item;
+        }
+    }
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Стек не закруэжен.");
+            return 0;
+        } else {
+            return stck[tos--];
+        }
+    }
+}
