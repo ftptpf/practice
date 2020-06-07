06ca69e244a9c4f357c5e062f0f0ed2d6e39dc7e
ftptpf <Nb49Rtdd>
2020-06-07 22:25:55 +0300

    Recursion - #196 Schildt

---
+++ src/ru/practice/schildt/other/Factorial.java
@@ -0,0 +1,10 @@
+package ru.practice.schildt.other;
+
+public class Factorial {
+    int fact (int n) {
+        int result;
+        if(n == 1) return 1;
+        result = fact(n - 1) * n;
+        return result;
+    }
+}
