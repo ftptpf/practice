9d54048133a743058f6c2ec009c62fc57de85a16
ftptpf <Nb49Rtdd>
2020-06-08 20:47:05 +0300

    Static -2  - #204 Schildt

---
+++ src/ru/practice/schildt/other/StaticDemo.java
@@ -0,0 +1,9 @@
+package ru.practice.schildt.other;
+
+public class StaticDemo {
+    static int a = 42;
+    static int b = 99;
+    static void callme() {
+        System.out.println("a = " +a);
+    }
+}
