9d5491bef13ab8269da268b607f38df1a599cd82
ftptpf <Nb49Rtdd>
2020-10-13 21:51:47 +0300

    2. Одинаковые элементы для массивов

---
+++ src/main/java/job4j/array/CrossArray.java
@@ -0,0 +1,18 @@
+package job4j.array;
+
+public class CrossArray {
+    /**
+     * Выводим в консоль числа, которые есть одновременно в первом и втором массиве.
+     * @param left
+     * @param right
+     */
+    public static void printCrossEl(int[] left, int[] right) {
+        for (int j : left) {
+            for (int i : right) {
+                if ( j == i) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
