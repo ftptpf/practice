2ba16c7f71ec026b2a3d976276f8f3cfe866a819
ftptpf <Nb49Rtdd>
2020-10-13 21:42:43 +0300

    1. Проверить сортировку массива

---
+++ src/main/java/job4j/array/Sorted.java
@@ -0,0 +1,17 @@
+package job4j.array;
+
+public class Sorted {
+    /**
+     * Метод проверяет отсортирован ли массив по возрастанию.
+     */
+    public static boolean isSorted(int[] array) {
+        boolean result = true;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
