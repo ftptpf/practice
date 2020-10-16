9b121f7b103610e52b0563ba88f8723002007db0
ftptpf <Nb49Rtdd>
2020-10-16 21:48:06 +0300

    4. Найти индекс элемента

---
+++ src/main/java/job4j/collection/UniqueElement.java
@@ -0,0 +1,23 @@
+package job4j.collection;
+
+import java.util.List;
+
+public class UniqueElement {
+    /**
+     * Метод принимает список и элемент который мы ищем в списке.
+     * Определяем, является ли этот элемент уникальным в этом списке.
+     * Для это необходимо находим первый и последний индекс вхождения элемента.
+     * Если равны - значит элемент уникальный. Предусматриваем ситуацию,
+     * что такого элемента нет вообще в коллекции, в этом случае возвращаем false.
+     * @param list
+     * @param str
+     * @return
+     */
+    public static boolean checkList(List<String> list, String str) {
+        boolean result = false;
+        if (list.contains(str) && (list.indexOf(str) == list.lastIndexOf(str))) {
+            result = true;
+        }
+        return result;
+    }
+}
