e2d70725f7619a094a092465228e0b8a4419bfcf
ftptpf <Nb49Rtdd>
2020-10-17 15:18:48 +0300

    8. Удаление одного списка из другого

---
+++ src/main/java/job4j/collection/SplitterList.java
@@ -0,0 +1,19 @@
+package job4j.collection;
+
+import java.util.List;
+
+public class SplitterList {
+    /**
+     * Метод  возращает список, основой которого будет первый, присутствовать элементы из второго
+     * и точно отсутствовать элементы из третьего.
+     * @param left Общий список элементов
+     * @param middle Список важных элементов, но не обязательных
+     * @param right Список элементов, которые обязательно должны отсутствовать в результирующем списке
+     * @return
+     */
+    public static List<String> split(List<String> left, List<String> middle, List<String> right) {
+        left.retainAll(middle);
+        left.removeAll(right);
+        return left;
+    }
+}
