f4d07dfc89bdb5a4105368af529923a039996e37
ftptpf <Nb49Rtdd>
2020-10-17 18:17:35 +0300

    13. Создание списка.

---
+++ src/main/java/job4j/collectionList/CreatorNewList.java
@@ -0,0 +1,25 @@
+package job4j.collectionList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreatorNewList {
+    /**
+     * Метод демонстрирует работу списков по добавлению значений и выоду на консоль.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Создаем список с пустым конструктором.");
+        List<String> first = new ArrayList<>();
+        System.out.println("Добавляем 3 элемента в список.");
+        first.add("one");
+        first.add("two");
+        first.add("three");
+        System.out.println("Создаем список - в конструктор передаем коллекцию.");
+        List<String> second = new ArrayList<>(first);
+        System.out.println("Выводим все элементы в списке.");
+        for (String el : second) {
+            System.out.println(el);
+        }
+    }
+}
