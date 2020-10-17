a6f284ee56ef3d2e00ab529c0ec044fb1a8f383c
ftptpf <Nb49Rtdd>
2020-10-17 15:04:18 +0300

    6. Удаление и замена элементов в списке - comments

--- src/main/java/job4j/collection/RepositionElement.java
+++ src/main/java/job4j/collection/RepositionElement.java
@@ -3,6 +3,15 @@ package job4j.collection;
 import java.util.List;
 
 public class RepositionElement {
+    /**
+     * Метод принимает в качестве параметров список и индекс элемента который необходимо заменить.
+     * Сначала удаляется последний элемент в списке, а потом этим элементом заменяется элемент по индексу,
+     * который пришел в параметрах метода. Учтено, что извне могут передать индекс,
+     * который превышает значение размера списка.
+     * @param list
+     * @param index
+     * @return
+     */
     public static List<String> changePosition(List<String> list, int index) {
         String a = list.remove(list.size() - 1);
         if (index < list.size()) {
