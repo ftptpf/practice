0a23042b1445cf6e5a8dde9231fcb290b6732446
ftptpf <Nb49Rtdd>
2020-10-17 17:50:30 +0300

    11. Итератор для списка

---
+++ src/main/java/job4j/collection/IteratorForList.java
@@ -0,0 +1,20 @@
+package job4j.collection;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorForList {
+    /**
+     *  Метод проходит по всему списку, выводит информацию что следующий элемент есть(в виде значения true),
+     *  а также значение этого элемента. Также после обхода списка выводит информацию, что больше элементов нет.
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<String> list = List.of("one", "two", "three", "four", "five");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Next element has - " + iterator.hasNext() + ", this element is " + iterator.next());
+        }
+        System.out.println("Next element has - " + iterator.hasNext());
+    }
+}
