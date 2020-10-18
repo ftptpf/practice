8fcb3a264ea20ef4a4aff7a99782eb7ea924bcc0
ftptpf <Nb49Rtdd>
2020-10-18 14:42:21 +0300

    1.3. Получение компаратора. Метод nullsFirst() - comments

--- src/main/java/job4j/sort/NullFirstMethod.java
+++ src/main/java/job4j/sort/NullFirstMethod.java
@@ -3,6 +3,10 @@ package job4j.sort;
 import java.util.Comparator;
 
 public class NullFirstMethod {
+    /**
+     * Метод перемещает все null строки в начало.
+     * @return
+     */
     public static Comparator<String> nullFirst() {
         return Comparator.nullsFirst(Comparator.naturalOrder());
     }
