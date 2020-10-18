6565da635286d359898ba6fad6f6f5afbfd5f830
ftptpf <Nb49Rtdd>
2020-10-18 14:35:25 +0300

    1.1. Получение компаратора. Метод naturalOrder() - comments

--- src/main/java/job4j/sort/NaturalOrder.java
+++ src/main/java/job4j/sort/NaturalOrder.java
@@ -3,6 +3,10 @@ package job4j.sort;
 import java.util.Comparator;
 
 public class NaturalOrder {
+    /**
+     * Метод выполняет прямую сортировку для строки.
+     * @return
+     */
     public static Comparator<String> naturalOrder() {
         return Comparator.naturalOrder();
     }
