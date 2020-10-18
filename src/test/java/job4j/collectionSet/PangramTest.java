59a71bdca3f564d840086408a635cd3d91564cf5
ftptpf <Nb49Rtdd>
2020-10-18 11:59:10 +0300

    3. Панграмма

---
+++ src/test/java/job4j/collectionSet/PangramTest.java
@@ -0,0 +1,16 @@
+package job4j.collectionSet;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PangramTest {
+
+    @Test
+    public void checkString() {
+        String str = "Jackdaws love my big sphinx of quartz";
+        boolean b = Pangram.checkString(str);
+        assertThat(b, is(true));
+    }
+}
