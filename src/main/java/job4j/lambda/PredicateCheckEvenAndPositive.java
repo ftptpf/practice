02c1aa5a942d0a6f9dea4e31a7617db3e67913c1
ftptpf <Nb49Rtdd>
2020-10-23 22:21:09 +0300

    1.3. Predicate. Проверить, что число положительное и четное

---
+++ src/main/java/job4j/lambda/PredicateCheckEvenAndPositive.java
@@ -0,0 +1,18 @@
+package job4j.lambda;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckEvenAndPositive {
+    /**
+     * Метод с помощью лямбда выражения проверяет что число четное и положительное.
+     * @param num
+     * @return
+     */
+    public static boolean check(int num) {
+        return check(x -> (x > 0) && (x % 2 == 0), num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
