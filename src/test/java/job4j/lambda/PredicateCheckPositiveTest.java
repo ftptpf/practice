222e6c0ad8b80e63a3ae65c9c955123eeb08cc15
ftptpf <Nb49Rtdd>
2020-10-23 22:07:57 +0300

    1.1. Predicate. Проверить, что число положительное

---
+++ src/test/java/job4j/lambda/PredicateCheckPositiveTest.java
@@ -0,0 +1,15 @@
+package job4j.lambda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PredicateCheckPositiveTest {
+
+    @Test
+    public void whenPositive() {
+        assertTrue(PredicateCheckPositive.check(1));
+        assertFalse(PredicateCheckPositive.check(-1));
+        assertFalse(PredicateCheckPositive.check(0));
+    }
+}
