621008be8d7f41adb30d82063ec8c4e88ed50be3
ftptpf <Nb49Rtdd>
2020-10-25 18:11:50 +0300

    4.3. Method reference и Predicate

---
+++ src/main/java/job4j/lambda/MRPredicate.java
@@ -0,0 +1,14 @@
+package job4j.lambda;
+
+import java.util.function.Predicate;
+
+public class MRPredicate {
+    /**
+     * Создаем предикат Predicate, который проверяет, что строка пустая,
+     * для этого используем метод isEmpty() в классе String.
+     * @return
+     */
+    public static Predicate<String> predicate() {
+        return String::isEmpty;
+    }
+}
