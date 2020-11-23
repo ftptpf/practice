f1c9bcbfe509fb4f0bee069b6e743ba1dba7efd8
ftptpf <Nb49Rtdd>
2020-11-23 20:29:49 +0300

    0.1. Понятие редукции. Произвольный Collector для сборки в коллекцию

---
+++ src/test/java/job4j/stream/CollectorClassTest.java
@@ -0,0 +1,16 @@
+package job4j.stream;
+
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class CollectorClassTest {
+
+    @Test
+    public void test() {
+        assertEquals(
+                List.of(1, 2, 3),
+                CollectorClass.collect(List.of(1, 2, 3))
+        );
+    }
+}
