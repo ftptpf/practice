1376dfffaa3c1e684c9a6aab404323fa84a4e536
ftptpf <Nb49Rtdd>
2020-11-24 21:45:35 +0300

    1.2. Создание стрима из последовательности. Метод Stream.of()

---
+++ src/main/java/job4j/stream/StreamOfMethod.java
@@ -0,0 +1,13 @@
+package job4j.stream;
+
+import java.util.stream.Stream;
+
+public class StreamOfMethod {
+    /**
+     * Метод создает стрим из символов 'a', 'b', 'c'.
+     * @return
+     */
+    public static Stream<Character> createStream() {
+        return Stream.of('a', 'b', 'c');
+    }
+}
