f7662a2aefb6462e3aec91778e83602c35e4c617
ftptpf <Nb49Rtdd>
2020-11-24 21:40:19 +0300

    1. flatMap для итератора

---
+++ src/main/java/job4j/stream/FlatIt.java
@@ -0,0 +1,24 @@
+package job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FlatIt {
+    /**
+     * Метод собирает все элементы итераторов в LIST.
+     * @param it Вложенные итераторы.
+     * @return
+     */
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        List<Integer> result = new ArrayList<>();
+        Iterator<Integer> prom;
+        while (it.hasNext()) {
+            prom = it.next();
+            while (prom.hasNext()) {
+                result.add(prom.next());
+            }
+        }
+        return result;
+    }
+}
