cfc0423fe4d9fc0be5f143eda4b042596334ef5b
ftptpf <Nb49Rtdd>
2020-11-24 22:04:22 +0300

    1.5. Создание стрима. Метод Stream.builder()

---
+++ src/main/java/job4j/stream/StreamBuilder.java
@@ -0,0 +1,14 @@
+package job4j.stream;
+
+import java.util.stream.Stream;
+
+public class StreamBuilder {
+    /**
+     * Метод создает стрим из первого и последнего элементов массива.
+     * @param data
+     * @return
+     */
+    public static Stream<Object> createStream(Integer[] data) {
+        return Stream.builder().add(data[0]).add(data[data.length - 1]).build();
+    }
+}
