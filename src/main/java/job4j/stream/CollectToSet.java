cc60ffb15503cf81c18030f132298b6c6a1ded14
ftptpf <Nb49Rtdd>
2020-11-28 22:48:43 +0300

    2.2. Сборка элементов. Сборка в Set

---
+++ src/main/java/job4j/stream/CollectToSet.java
@@ -0,0 +1,16 @@
+package job4j.stream;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToSet {
+    /**
+     * Метод собирает переданный стрим в SET.
+     * @param data
+     * @return
+     */
+    public static Set<Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toSet());
+    }
+}
