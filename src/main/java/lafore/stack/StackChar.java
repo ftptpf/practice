19864ece0195a10b46e5b5caccb9a02e7032d3d5
ftptpf <Nb49Rtdd>
2021-02-22 20:02:38 +0300

    Stack

---
+++ src/main/java/lafore/stack/StackChar.java
@@ -0,0 +1,47 @@
+package lafore.stack;
+
+/**
+ * Реализация стека на массиве символов.
+ */
+public class StackChar {
+    private int maxSize;
+    private char[] stackArray;
+    private int top;
+
+    public StackChar(int max) {
+        maxSize = max;
+        stackArray = new char[maxSize];
+        top = - 1;
+    }
+    /**
+     * Размещаем элемент на вершине стека.
+     * @param j
+     */
+    public void push(char j) {
+        stackArray[++top] = j;
+    }
+
+    /**
+     * Извлекаем элемент с вершины стека.
+     * @return
+     */
+    public char pop() {
+        return stackArray[top--];
+    }
+
+    /**
+     * Чтение элемента с вершины стека.
+     * @return
+     */
+    public char peek() {
+        return stackArray[top];
+    }
+
+    /**
+     * True, если стек пустой.
+     * @return
+     */
+    public boolean isEmpty() {
+        return top == -1;
+    }
+}
--- src/main/java/main/java/schildt/other/TestStack.java
+++ src/main/java/main/java/schildt/other/TestStack.java
@@ -2,8 +2,8 @@ package main.java.schildt.other;
 
 public class TestStack {
     public static void main(String[] args) {
-        Stack mystack1 = new Stack(3);
-        Stack mystack2 = new Stack(3);
+        main.java.schildt.other.Stack mystack1 = new main.java.schildt.other.Stack(3);
+        main.java.schildt.other.Stack mystack2 = new main.java.schildt.other.Stack(3);
         for (int i = 0; i < 10; i++) mystack1.push(i);
         for (int i = 10; i < 20; i++) mystack2.push(i);
         System.out.println("Содержимое стека mystack1: ");
