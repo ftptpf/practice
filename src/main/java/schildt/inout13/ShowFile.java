00f34d259b556410f4589f33812801d955bd0d73
ftptpf <Nb49Rtdd>
2021-04-05 21:24:30 +0300

    Глава 13. Ввод - вывод. ShowFiles - FileNotFoundException

--- src/main/java/schildt/inout13/ShowFile.java
+++ src/main/java/schildt/inout13/ShowFile.java
@@ -12,32 +12,32 @@ import java.io.IOException;
 public class ShowFile {
     public static void main(String[] args) {
         int i;
-        FileInputStream fin;
+        FileInputStream fin = null;
         // Убедимся что имя файла указано
         if (args.length != 1) {
             System.out.println("Использование: ShowFile имя_файла. ");
         }
-        // Попытка открыть файл
+        // Сначала открывается файл,
+        // а затем из него читаются символы до тех пор,
+        // пока не встретится признак конца файла
         try {
             fin = new FileInputStream(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.println("Невозможно открыть файл.");
-        }
-        // Далее читаем символы до тех пор,
-        // пока не встретим признак конца файла
-        try {
             do {
-                i = fin.read();
-                if (i != -1) {
+               i = fin.read();
+               if (i != -1) {
                     System.out.print((char) i);
-                }
+               }
             } while (i != -1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
         } catch (IOException e) {
-            System.out.println("Ошибка чтения из файла.");
+            System.out.println("Произошла ошибка ввода-вывода.");
         } finally {
-            // закрыть файл
+            // закрыть файл в любом случае
             try {
-                fin.close();
+                if (fin != null) {
+                    fin.close();
+                }
             } catch (IOException e) {
                 System.out.println("Ошибка закрытия файла.");
             }
